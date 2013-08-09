c90ecdc23d52f51cdb7de6aff092824af5d2dd0d
Zach Tellman <ztellman@gmail.com>
2013-08-09 15:10:48 -0700

    add sorted-interleave-by back in

--- src/skuld/util.clj
+++ src/skuld/util.clj
@@ -7,7 +7,7 @@
      Collection
      PriorityQueue]))
 
-(deftype SeqContainer [^long idx s]
+(deftype SeqContainer [key-fn ^long idx s]
   Comparable
   (equals [_ x]
     (and
@@ -16,12 +16,12 @@
       (identical? (.s ^SeqContainer x) s)))
   (compareTo [_ x]
     (let [^SeqContainer x x
-          cmp (compare (first s) (first (.s x)))]
+          cmp (compare (key-fn (first s)) (key-fn (first (.s x))))]
       (if (p/zero? cmp)
         (compare idx (.idx x))
         cmp))))
 
-(defn- sorted-interleave- [^PriorityQueue heap]
+(defn- sorted-interleave- [key-fn ^PriorityQueue heap]
   (lazy-seq
     (loop [chunk-idx 0, buf (chunk-buffer 32)]
       (if (.isEmpty heap) 
@@ -29,19 +29,25 @@
         (let [^SeqContainer container (.poll heap)]
           (chunk-append buf (first (.s container)))
           (when-let [s' (seq (rest (.s container)))]
-            (.offer heap (SeqContainer. (.idx container) s')))
+            (.offer heap (SeqContainer. key-fn (.idx container) s')))
           (let [chunk-idx' (unchecked-inc chunk-idx)]
             (if (< chunk-idx' 32)
               (recur chunk-idx' buf)
               (chunk-cons
                 (chunk buf)
-                (sorted-interleave- heap)))))))))
+                (sorted-interleave- key-fn heap)))))))))
+
+(defn sorted-interleave-by
+  "Like sorted-interleave, but takes a specific keyfn, like sort-by."
+  [key-fn & seqs]
+  (sorted-interleave-
+    key-fn
+    (PriorityQueue.
+      ^Collection
+      (map #(SeqContainer. key-fn %1 %2) (range) (remove empty? seqs)))))
 
 (defn sorted-interleave
   "Given n sorted sequences, yields a lazy sequence which yields all elements
   in all n collections, in order."
   [& seqs]
-  (sorted-interleave-
-    (PriorityQueue.
-      ^Collection
-      (map #(SeqContainer. %1 %2) (range) (remove empty? seqs)))))
+  (apply sorted-interleave-by identity seqs))
