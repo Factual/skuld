af4a6616bef07d9482114e5ac51a2ab64dc63bc0
Eric Lindvall <eric@5stops.com>
2013-09-04 12:41:39 -0700

    Don't mess with log4j yet

--- src/skuld/logging.clj
+++ src/skuld/logging.clj
@@ -3,15 +3,7 @@
   ; With thanks to arohner
   (:import (org.apache.log4j
              Logger
-             BasicConfigurator
-             PatternLayout
-             Level
-             ConsoleAppender
-             FileAppender
-             SimpleLayout
-             RollingFileAppender)
-           (org.apache.log4j.spi RootLogger))
-  (:import org.apache.commons.logging.LogFactory))
+             Level)))
 
 (def levels {:trace org.apache.log4j.Level/TRACE
              :debug org.apache.log4j.Level/DEBUG
@@ -46,56 +38,3 @@
   "Turns off logging for the evaluation of body."
   [loggers & body]
   (with-level :fatal loggers body))
-
-(def skuld-layout
-  "A nice format for log lines."
-  (PatternLayout. "%p [%d] %t - %c - %m%n%xEx%n"))
-
-(defn init
-  "Initialize log4j. You will probably call this from the config file. You can
-  call init more than once; its changes are destructive. Options:
-
-  :file   The file to log to. If omitted, logs to console only."
-  [& { :keys [file] }]
-  ; Reset loggers
-  (doto (Logger/getRootLogger)
-    (.removeAllAppenders)
-    (.addAppender (ConsoleAppender. skuld-layout)))
-
-  (comment
-  (when file
-    (let [rolling-policy (doto (TimeBasedTriggeringPolicy.)
-                           (.setActiveFileName file)
-                           (.setFileNamePattern
-                             (str file ".%d{yyyy-MM-dd}.gz"))
-                           (.activateOptions))
-          log-appender (doto (RollingFileAppender.)
-                         (.setRollingPolicy rolling-policy)
-                         (.setLayout skuld-layout)
-                         (.activateOptions))]
-      (.addAppender (Logger/getRootLogger) log-appender)))
-    )
-
-  ; Set levels.
-  (set-level :info)
-
-  (set-level "skuld.node" :debug)
-
-; Not sure where he intended this to go....
-(defn- add-file-appender [loggername filename]
-  (.addAppender (Logger/getLogger loggername)
-    (doto (FileAppender.)
-      (.setLayout skuld-layout))))
-
-  (comment
-(defn nice-syntax-error
-  "Rewrites clojure.lang.LispReader$ReaderException to have error messages that
-  might actually help someone."
-  ([e] (nice-syntax-error e "(no file)"))
-  ([e file]
-    ; Lord help me.
-    (let [line (wall.hack/field (class e) :line e)
-          msg (.getMessage (or (.getCause e) e))]
-      (RuntimeException. (str "Syntax error (" file ":" line ") " msg))))))
-
-  )
