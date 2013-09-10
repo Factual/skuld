c68dd5c3c3d790f8b1967f07e9d7bfea33467e9d
Eric Lindvall <eric@5stops.com>
2013-09-10 01:42:07 -0700

    Fix logging

--- src/skuld/logging.clj
+++ src/skuld/logging.clj
@@ -3,7 +3,8 @@
   (:import (ch.qos.logback.classic
              Level
              Logger
-             LoggerContext)
+             LoggerContext
+             PatternLayout)
            (org.slf4j
              LoggerFactory)
            (ch.qos.logback.classic.jul
@@ -27,7 +28,6 @@
 
 (defn ^Logger get-logger
   [^String logger-name]
-  (prn :get-logger logger-name)
   (LoggerFactory/getLogger
     (or logger-name root-logger-name)))
 
@@ -48,18 +48,18 @@
   ([level]
     (set-level nil level))
   ([^Logger logger level]
-    (prn :set-level logger level)
     (.setLevel (get-logger logger) (level-for level))))
 
 (defmacro with-level
   "Sets logging for the evaluation of body to the desired level."
   [level-name logger-names & body]
   `(let [level# (level-for ~level-name)
-         loggers-and-levels# ((->> [~logger-names]
+         root-logger-level# (.getLevel root-logger)
+         loggers-and-levels# (doall
+                               (->> [~logger-names]
                                 flatten
                                 (map get-logger)
-                                (map #(list % (.getLevel ^Logger %)))
-                                (remove (comp nil? second))))]
+                                (map #(list % (or (.getLevel ^Logger %) root-logger-level#)))))]
      (try
        (doseq [[^Logger logger# orig-level#] loggers-and-levels#]
          (.setLevel logger# level#))
@@ -71,28 +71,32 @@
 (defmacro mute
   "Turns off logging for all loggers the evaluation of body."
   [& body]
-  (with-level :off (all-logger-names) ~@body))
+  `(with-level :off (all-logger-names) ~@body))
 
 (defmacro suppress
   "Turns off logging for the evaluation of body."
   [loggers & body]
-  (with-level :off loggers ~@body))
+  `(with-level :off ~loggers ~@body))
 
 (defn init
   []
-  (let [context (.getLoggerContext root-logger)]
-    (doto context
-      .reset
-      (.addListener
-        (doto (LevelChangePropagator.)
-          (.setContext context)
-          (.setResetJUL true))))
-    (.addAppender root-logger
-      (doto (ConsoleAppender.)
-        (.setName "console-appender")
-        (.setContext context)
-        (.start))
-      )
-    (set-level :info)))
+  (doto root-logger-context
+    .reset
+    (.addListener
+      (doto (LevelChangePropagator.)
+        (.setContext root-logger-context)
+        (.setResetJUL true))))
+  (.addAppender root-logger
+    (doto (ConsoleAppender.)
+      (.setName "console-appender")
+      (.setContext root-logger-context)
+      (.setLayout
+        (doto (PatternLayout.)
+          (.setContext root-logger-context)
+          (.setPattern "%date{YYYY-MM-dd'T'HH:mm:ss.SSS} %-5p %c: %m%n%xEx")
+          (.start)))
+      (.start))
+    )
+  (set-level :info))
 
 (init)
