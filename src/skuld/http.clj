19d70c5e313a42a48f874ac22a8b5c392f8917c4
Max Countryman <max.countryman@gmail.com>
2013-11-04 17:55:52 -0800

    add initial accept header checking; add content-type response header

--- src/skuld/http.clj
+++ src/skuld/http.clj
@@ -19,12 +19,22 @@
    :headers (or headers {})
    :body body})
 
+(def ^:private ok-response (partial http-response 200))
+
+(defn- serialize
+  [req resp-body]
+  (let [accept (get-in req [:headers "Accept"] "application/json")
+        json-ct {"Content-Type" "application/json;charset=utf-8"}]
+    (condp re-find accept
+      #"^application/(vnd.+)?json" [(json/generate-string resp-body) json-ct]
+      [(json/generate-string resp-body) json-ct])))
+
 (defn- endpoint
   "Defines an HTTP endpoint with an allowed request method. Takes an allowed
   method, a request map, and the response body."
   [allowed-method req resp-body]
   (if (= (:request-method req) allowed-method)
-    (http-response 200 resp-body)
+    (apply ok-response (serialize req resp-body))
     (http-response 405 "Method Not Allowed")))
 
 (def ^:private GET (partial endpoint :get))
@@ -34,9 +44,9 @@
   [node]
   (fn [req]
     (condp route-matches req
-      "/count_queue" (GET req (json/generate-string (node/count-queue node {})))
-      "/count_tasks" (GET req (json/generate-string (node/count-tasks node {})))
-      "/list_tasks"  (GET req (json/generate-string (node/list-tasks node {})))
+      "/count_queue" (GET req (node/count-queue node {}))
+      "/count_tasks" (GET req (node/count-tasks node {}))
+      "/list_tasks"  (GET req (node/list-tasks node {}))
       (http-response 404 "Not Found"))))
 
 (defn service
