f87a33a14dae4b62d49cd3900e94317a92db1921
Kyle Kingsbury <kingsbury@factual.com>
2013-11-25 17:46:59 -0800

    Fix josql missing repo

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :java-source-paths ["src/skuld/"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["jboss-fs-public" "http://repository.jboss.org/nexus/content/groups/fs-public/"]]
   :exclusions [[log4j]
                [org.slf4j/slf4j-log4j12]]
   :dependencies [[org.clojure/clojure "1.5.1"]
